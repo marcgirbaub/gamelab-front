31efeb00bdef60d05d237a6d6a6cd7380fd472eb
Marc Girbau <marc.girbau94@gmail.com>
2023-03-07 20:37:38 +0100

    Add global padding and colors

---
+++ src/styles/colorStyles.ts
@@ -0,0 +1,12 @@
+const colorStyles = {
+  main: "#0f0f0f",
+  primaryLight: "#3f3f3f",
+  accent: "#ff6d74",
+  secondary: "#ffffff",
+  secondaryLight: "#aaaaaa",
+  error: "#ff9100",
+  success: "#84e14d",
+  cardLight: "#202020",
+};
+
+export default colorStyles;
