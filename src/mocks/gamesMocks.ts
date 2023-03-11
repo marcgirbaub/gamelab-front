26772876b882e848f94a82f18894a820cd727d73
Marc Girbau <marc.girbau94@gmail.com>
2023-03-11 20:07:16 +0100

    Test loadAllGames reducer case

---
+++ src/mocks/gamesMocks.ts
@@ -0,0 +1,29 @@
+import { type Game, type Games } from "../store/features/gamesSlice/types";
+
+export const mockWitcherGame: Game = {
+  id: "123123",
+  name: "The Witcher",
+  about: "This is an adventure game",
+  ageRating: "17 +",
+  backupImage: "thewitcher.jpg",
+  categories: ["Adventure", "Action"],
+  developer: "EA games",
+  gameplayTime: 60,
+  platforms: ["Playstation"],
+  releaseYear: 2015,
+};
+
+export const mockLeagueGame: Game = {
+  id: "123123",
+  name: "League of Legends",
+  about: "This is a 5 vs 5 games",
+  ageRating: "17 +",
+  backupImage: "lol.jpg",
+  categories: ["Adventure", "Action"],
+  developer: "Riot",
+  gameplayTime: 100,
+  platforms: ["Windows"],
+  releaseYear: 2010,
+};
+
+export const mockListOfGames: Games = [mockWitcherGame, mockLeagueGame];
