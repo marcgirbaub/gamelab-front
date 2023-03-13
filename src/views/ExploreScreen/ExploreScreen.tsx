b492f435e1c7adfd87b79077b16013991c4cf485
Marc Girbau <marc.girbau94@gmail.com>
2023-03-13 15:23:59 +0100

    Implement Filter in ExploreScreen

--- src/views/ExploreScreen/ExploreScreen.tsx
+++ src/views/ExploreScreen/ExploreScreen.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import React, { useEffect } from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import Filter from "../../components/Filter/Filter";
 import GamesList from "../../components/GamesList/GamesList";
 import Header from "../../components/Header/Header";
@@ -26,7 +26,11 @@ const ExploreScreen = () => {
       <Header />
       <View style={exploreScreenStyles.container}>
         <Filter />
-        <GamesList games={games} />
+        {games.length === 0 ? (
+          <Text style={exploreScreenStyles.notFoundText}>No games found</Text>
+        ) : (
+          <GamesList games={games} />
+        )}
       </View>
     </SafeAreaView>
   );
