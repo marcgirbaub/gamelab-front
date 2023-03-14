3f4d7315229ad2686eff172975ba036c09b32903
Marc Girbau <marc.girbau94@gmail.com>
2023-03-14 22:53:14 +0100

    Test loadOneGame reducer case

--- src/redux/features/games/gamesSlice.test.ts
+++ src/redux/features/games/gamesSlice.test.ts
@@ -8,12 +8,14 @@ import {
   gamesReducer,
   loadAllGamesActionCreator,
   loadMoreGamesActionCreator,
+  loadOneGameActionCreator,
 } from "./gamesSlice";
+import { type GamesState } from "./types";
 
 describe("Given a gamesReducer reducer", () => {
   describe("When it receives the action to load two games", () => {
     test("Then it should return a new state with a list of games with these two games in it", () => {
-      const expectedNewState = {
+      const expectedNewState: GamesState = {
         ...initialGamesStateMock,
         games: mockListOfGames,
       };
@@ -27,13 +29,27 @@ describe("Given a gamesReducer reducer", () => {
 
   describe("When it receives the action to load one more game", () => {
     test("Then it should return a new state with previous list of games and the two additional games", () => {
-      const expectedNewState = {
+      const expectedNewState: GamesState = {
         ...initialGamesStateMock,
         games: [...initialGamesStateMock.games, mockValorantGame],
       };
 
       const loadMoreAction = loadMoreGamesActionCreator([mockValorantGame]);
       const newState = gamesReducer(initialGamesStateMock, loadMoreAction);
+
+      expect(newState).toStrictEqual(expectedNewState);
+    });
+  });
+
+  describe("When it receives the action to load one game", () => {
+    test("Then it should return a new state with the selectedGame property equal to the game loaded", () => {
+      const expectedNewState: GamesState = {
+        ...initialGamesStateMock,
+        selectedGame: { ...mockValorantGame },
+      };
+
+      const loadOneGameAction = loadOneGameActionCreator(mockValorantGame);
+      const newState = gamesReducer(initialGamesStateMock, loadOneGameAction);
 
       expect(newState).toStrictEqual(expectedNewState);
     });
